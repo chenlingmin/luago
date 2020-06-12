3fbd7e937fbfcf1b2b1c349f2ebd89f4e7367ccb
陈灵敏 <chenlingmin@mem853.com>
2020-06-12 16:34:09 +0800

    lua state

--- main.go
+++ main.go
@@ -2,22 +2,62 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"luago/api"
 	"luago/binchunk"
+	"luago/state"
 	"luago/vm"
-	"os"
 )
 
 func main() {
 
-	if len(os.Args) > 1 {
-		data, err := ioutil.ReadFile(os.Args[1])
-		if err != nil {
-			panic(err)
+	ls := state.New()
+
+	ls.PushBoolean(true)
+	printStack(ls)
+	ls.PushInteger(10)
+	printStack(ls)
+	ls.PushNil()
+	printStack(ls)
+	ls.PushString("hello")
+	printStack(ls)
+	ls.PushValue(-4)
+	printStack(ls)
+	ls.Replace(3)
+	printStack(ls)
+	ls.SetTop(6)
+	printStack(ls)
+	ls.Remove(-3)
+	printStack(ls)
+	ls.SetTop(-5)
+	printStack(ls)
+}
+
+//if len(os.Args) > 1 {
+//	data, err := ioutil.ReadFile(os.Args[1])
+//	if err != nil {
+//		panic(err)
+//	}
+//	proto := binchunk.Undump(data)
+//	list(proto)
+//}
+//}
+
+func printStack(ls api.LuaState) {
+	top := ls.GetTop()
+	for i := 1; i <= top; i++ {
+		t := ls.Type(i)
+		switch t {
+		case api.LUA_TBOOLEAN:
+			fmt.Printf("[%t]", ls.ToBoolean(i))
+		case api.LUA_TNUMBER:
+			fmt.Printf("[%g]", ls.ToNumber(i))
+		case api.LUA_TSTRING:
+			fmt.Printf("[%q]", ls.ToString(i))
+		default: // other values
+			fmt.Printf("[%s]", ls.TypeName(t))
 		}
-		proto := binchunk.Undump(data)
-		list(proto)
 	}
+	fmt.Println()
 }
 
 func list(f *binchunk.Prototype) {
---
+++ state/lua_value.go
@@ -0,0 +1,31 @@
+package state
+
+import "luago/api"
+
+type luaValue interface{}
+
+func typeOf(val luaValue) api.LuaType {
+	switch val.(type) {
+	case nil:
+		return api.LUA_TNIL
+	case bool:
+		return api.LUA_TBOOLEAN
+	case int64, float64:
+		return api.LUA_TNUMBER
+	case string:
+		return api.LUA_TSTRING
+	default:
+		panic("todo!")
+	}
+}
+
+func convertToBoolean(val luaValue) bool {
+	switch x := val.(type) {
+	case nil:
+		return false
+	case bool:
+		return x
+	default:
+		return true
+	}
+}
